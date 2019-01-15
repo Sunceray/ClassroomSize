a0cfd68e1adc56cee60160eefb89a1a40a484dc3
sunceray <sunceraypatterson@gmail.com>
2019-01-15 16:11:11 -0500

    int commit

---
+++ src/Lab2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Lab2 {
+	
+	public static void main (String[] args) {
+		  String choice = "y";
+		  while (choice.equalsIgnoreCase("y")) {
+
+		
+		System.out.println("Welcome To Grand Circus Room Detail Generator");
+		
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+		System.out.println("Enter Length:");
+		double rmLength = input.nextDouble();
+		
+		System.out.println("Enter Width:");
+		 double rmWidth = input.nextDouble();
+		 
+		 System.out.println("Enter Height:");
+		 double height = input.nextDouble();
+		
+	
+   	    double area = rmLength * rmWidth;
+   	    System.out.println("Area of GC Rooms:" + area);
+   	 
+		
+		double perimeter = 2* (rmLength * rmWidth);
+		System.out.println("Perimeter of GC Rooms:" + perimeter);
+		
+		
+		double volume = rmLength*rmWidth*height;
+		System.out.println("Volume:"+volume);
+		
+	
+		System.out.print("Continue? (y/n): ");
+        choice = input.next();
+		  }
+	}
+
+	}
+
+
