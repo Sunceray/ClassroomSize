3f2bda503eb87791938cc0e3504c0b2d824ac346
sunceray <sunceraypatterson@gmail.com>
2019-03-20 22:08:53 -0400

    second commit

--- src/Lab2.java
+++ src/Lab2.java
@@ -4,39 +4,37 @@ public class Lab2 {
 	
 	public static void main (String[] args) {
 		  String choice = "y";
+		  
+		  System.out.println("Welcome To Grand Circus Room Detail Generator");
+			double height; //declaring
+			
+			Scanner input = new Scanner(System.in);
 		  while (choice.equalsIgnoreCase("y")) {
-
+			
+			System.out.println("Enter Length:");
+			double rmLength = input.nextDouble(); //Initializing 
+			
+			System.out.println("Enter Width:");
+			 double rmWidth = input.nextDouble();
+			 
+			 System.out.println("Enter Height:");
+			 double rmheight = input.nextDouble();
+			
 		
-		System.out.println("Welcome To Grand Circus Room Detail Generator");
-		
-		
-		Scanner input = new Scanner(System.in);
-		
-		
-		System.out.println("Enter Length:");
-		double rmLength = input.nextDouble();
-		
-		System.out.println("Enter Width:");
-		 double rmWidth = input.nextDouble();
+		    double area = rmLength * rmWidth;
+		    System.out.println("Area of GC Rooms:" + area);
 		 
-		 System.out.println("Enter Height:");
-		 double height = input.nextDouble();
+			
+			double perimeter = 2* (rmLength * rmWidth);
+			System.out.println("Perimeter of GC Rooms:" + perimeter);
+			
+			
+			double volume = rmLength*rmWidth*rmheight;
+			System.out.println("Volume:"+volume);
+			
 		
-	
-   	    double area = rmLength * rmWidth;
-   	    System.out.println("Area of GC Rooms:" + area);
-   	 
-		
-		double perimeter = 2* (rmLength * rmWidth);
-		System.out.println("Perimeter of GC Rooms:" + perimeter);
-		
-		
-		double volume = rmLength*rmWidth*height;
-		System.out.println("Volume:"+volume);
-		
-	
-		System.out.print("Continue? (y/n): ");
-        choice = input.next();
+			System.out.print("Continue? (y/n): ");
+		    choice = input.next();
 		  }
 	}
 
